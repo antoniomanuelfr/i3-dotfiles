d789e14c3e5eee0b5fe138fee97150a5aa332819
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2019-09-29 12:46:29 +0200

    polybar

--- launch.sh
+++ launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c ~/.config/config_files/polybar_config mybar &
+polybar -c ~/.config/config_files/polybar.conf mybar &
 
 echo "Polybar launched..."
